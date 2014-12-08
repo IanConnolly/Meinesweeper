0199bd97272a3ab44964497dbf2af7013de0444a
Fintan Halpenny <rossmanf@tcd.ie>
2014-12-08 17:52:03 +0000

    Few more cabal flags added, show instance for Field and Board, testing the printing of a board in main

--- src/Meinesweeper/Field.hs
+++ src/Meinesweeper/Field.hs
@@ -1,6 +1,13 @@
-module Meinesweeper.Field (Field (Field, mined, flagged, covered)) where
+module Meinesweeper.Field where
 
 data Field = Field { mined :: Bool
                    , flagged :: Bool
                    , covered :: Bool
-                   } deriving (Show)
+                   }
+
+instance Show Field where
+  show :: Field -> String
+  show f =
+    if flagged f then "| <| |" else
+       if covered f then "| [] |" else
+          if mined f then "| * |" else "| |"
