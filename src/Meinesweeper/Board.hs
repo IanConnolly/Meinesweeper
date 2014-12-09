809426a2177235a63c9ca6ba62c9113c8bb856bf
Ian Connolly <ian@connolly.io>
2014-12-09 10:31:47 +0000

    Lens-ify

--- src/Meinesweeper/Board.hs
+++ src/Meinesweeper/Board.hs
@@ -1,6 +1,6 @@
 module Meinesweeper.Board (Board, createBoard, flagSquare) where
 
-import Meinesweeper.Field
+import qualified Meinesweeper.Field as MF
 import Data.Maybe
 import Control.Lens
 import Control.Lens.At
@@ -9,7 +9,7 @@ import qualified Data.List as DL
 import Data.Vector hiding (modify)
 import Control.Monad.State 
 
-type Board = Vector (Vector Field)
+type Board = Vector (Vector MF.Field)
 type GameBoard = State Board
 type Height = Int
 type Width = Int
@@ -23,15 +23,32 @@ instance Show Board where
 
 createBoard :: Height -> Width -> Board -- Seed -> Height -> Width -> Board
 createBoard h w = insertBombs $ createEmptyBoard h w
-    where createEmptyBoard h w = replicate h $ replicate w $ newField
+    where createEmptyBoard h w = replicate h $ replicate w $ MF.newField
           insertBombs = map inserter
-          inserter = over (element (generateIndex w) . mined) (const True)
+          inserter = over (element (generateIndex w) . MF.mined) (const True)
 
-generateIndex :: Int -> Int
+generateIndex :: Int -> Int -- TODO: Dan's random index generator
 generateIndex x = x - 1
 
+isMined :: Int -> Int -> GameBoard Bool
+isMined x y = viewSquare x y MF.covered
+
+isCovered :: Int -> Int -> GameBoard Bool
+isCovered x y = viewSquare x y MF.covered
+
+uncover :: Int -> Int -> GameBoard ()
+uncover x y = modifySquare x y MF.covered False
+
+isFlagged :: Int -> Int -> GameBoard Bool
+isFlagged x y = viewSquare x y MF.flagged
+
 flagSquare :: Int -> Int -> GameBoard ()
-flagSquare x y = modify $ over (element y . element x . flagged) (const True)
+flagSquare x y = modifySquare x y MF.flagged True
 
 unflagSquare :: Int -> Int -> GameBoard ()
-unflagSquare x y = modify $ over (element y . element x . flagged) (const False)
+unflagSquare x y = modifySquare x y MF.flagged False
+
+modifySquare x y record val = modify $ over (element y . element x . record) (const val)
+viewSquare x y record = do
+    g <- get
+    return $ fromJust $ preview (element y . element x . record) $ g
