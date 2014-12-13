6516126730a9056a45268371a8022e519dd614ce
Ian Connolly <ian@connolly.io>
2014-12-13 13:47:08 +0000

    GHC bug?

--- src/Meinesweeper/Game.hs
+++ src/Meinesweeper/Game.hs
@@ -1,29 +1,38 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Meinesweeper.Game where
 
 import Meinesweeper.Board
 import Meinesweeper.Field
 import Control.Monad.State
-import Data.Vector as DV
+import qualified Data.Vector as DV
 import Data.Maybe
 import Control.Lens
 
 type Height = Int
 type Width = Int
 
-clickField :: Int -> Int -> GameBoard ()
+data Meinesweeper = Meinesweeper
+    { _flagsLeft :: Int
+    }
+
+makeLenses ''Meinesweeper
+
+type Game = StateT Meinesweeper (GameBoard) 
+
+clickField :: Int -> Int -> Game ()
 clickField x y = do
-    c <- isCovered x y
-    if c then checkForMine x y else doNothing
+    c <- lift isCovered x y
+    if c then lift checkForMine x y else return ()
 
-rightClickField :: Int -> Int -> GameBoard ()
+rightClickField :: Int -> Int -> Game ()
 rightClickField x y = do
-    f <- isFlagged x y
-    if f then flag x y else unflag x y
+    f <- lift isFlagged x y
+    if f then lift flag x y else lift unflag x y
 
-checkForMine :: Int -> Int -> GameBoard ()
+checkForMine :: Int -> Int -> Game ()
 checkForMine x y = do
-    m <- isMined x y
-    if m then uncoverAll else uncover x y
+    m <- lift isMined x y
+    if m then lift $ uncoverAll else lift uncover x y
 
 isWon :: GameBoard Bool
 isWon = do
