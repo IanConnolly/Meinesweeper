0700164658fa6cb850beed37afc14b8c64d6593f
Fintan Halpenny <rossmanf@tcd.ie>
2014-12-19 22:14:44 +0000

    clicking but nothing happening

--- src/Meinesweeper/Main.hs
+++ src/Meinesweeper/Main.hs
@@ -3,25 +3,26 @@ module Main where
 import Meinesweeper.Game
 import Meinesweeper.Field
 import Meinesweeper.Board
+import Control.Monad.State
 import System.Random
 import Graphics.UI.WX
 import qualified Data.Vector as DV
-import Control.Monad.State
+import System.IO
 
 --Easy height width and mines
-easyH = 8 :: Int
-easyW = 8 :: Int
-easyM = 10 :: Int
+easyH = 8
+easyW = 8
+easyM = 10
 
 --Medium height width and mines
-mediumH = 15 :: Int
-mediumW = 15 :: Int
-mediumM = 40 :: Int
+mediumH = 15
+mediumW = 15
+mediumM = 40
 
 --Hard height width and mines
-hardH = 15 :: Int
-hardW = 29 :: Int
-hardM = 99 :: Int
+hardH = 15
+hardW = 29
+hardM = 99
 
 newBoard :: String -> Int -> Int -> Int -> IO ()
 newBoard title h w m = do
@@ -29,27 +30,11 @@ newBoard title h w m = do
 
   g <- newStdGen
   gameState <- varCreate $ newMeinesweeper h w m g
-
-  game <- varGet gameState
-
-  --display board
-  let board = _board game
-  boardButtons <- boardGUI board f gameState
-  let gui = widgetise h boardButtons
-
-  quit <- button f [text := "Quit"
-                   ,on command := close f]
-  back <- button f [text := "Main"
-                   ,on command := (close f >> mainMenu)]
-
-  set f [layout := minsize (sz 200 100) $ margin 10 $ column 0 $ gui ++ [floatBottom $ widget back, floatBottom $ widget quit]]
-    where
-      widgetise :: Int -> [[Button ()]] -> [Layout]
-      widgetise _ [] = []
-      widgetise r (b:bs) = row r (map widget b) : widgetise r bs
+  makeGUI gameState f h
 
 mainMenu :: IO ()
 mainMenu = do
+
   f <- frameFixed [text := "MineSweeper"]
 
   --Widgets
@@ -71,25 +56,47 @@ mainMenu = do
                    ,floatCentre $ widget hard
                    ,floatCentre $ widget quit]]]
 
-fieldButton :: Frame () -> Var Meinesweeper -> Field  -> IO (Button ())
-fieldButton f gameState field
-  | _covered field = makeButton f "  " gameState
-  | _flagged field = makeButton f "<|" gameState
-  | _mined field   = makeButton f "**" gameState
-  | otherwise      = makeButton f "  " gameState
-    where
-      makeButton f txt g = do
-        game <- varGet g
-        smallButton f [text := txt
-                      ,on click := (\b -> let (x,y) = getPoint b
-                                          in varSet g (execState (leftClickField x y) game))
-                      ,on clickRight := (\b -> let (x,y) = getPoint b
-                                               in varSet g (execState (leftClickField x y) game))
-                      ]
-      getPoint p = (pointX p, pointY p)
+fieldButton :: Int -> Frame () -> Var Meinesweeper -> Field -> IO (Button ())
+fieldButton h f gameState field
+  | _flagged field = makeButton h f gameState "<|"
+  | _covered field = makeButton h f gameState "  "
+  | _mined field   = makeButton h f gameState "**"
+  | otherwise      = makeButton h f gameState (show $ _adjacentMines field)
+  where
+   makeButton h f g txt = do
+     game <- varGet g
+     smallButton f [text := txt
+                   ,on click := \p -> let (x,y) = getPoint p
+                                          (win,state) = runState (leftClickField x y) game
+                                      in varSet g state >> putStrLn (show x ++ " " ++ show y) ]{-->> makeGUI g f h]--}
+   getPoint p = (pointX p, pointY p)
 
-boardGUI :: Board -> Frame () -> Var Meinesweeper -> IO [[Button ()]]
-boardGUI b f g = mapM (mapM (fieldButton f g)) (DV.toList $ DV.map DV.toList b)
+boardGUI :: Board -> Int -> Frame () -> Var Meinesweeper -> IO [[Button ()]]
+boardGUI b h f g = mapM (mapM (fieldButton h f g)) (DV.toList $ DV.map DV.toList b)
+
+widgetise :: Int -> [[Button ()]] -> [Layout]
+widgetise _ [] = []
+widgetise r (b:bs) = row r (map widget b) : widgetise r bs
+
+makeGUI :: Var Meinesweeper -> Frame () -> Int -> IO ()
+makeGUI gameState f h = do
+  game <- varGet gameState
+
+  let board = _board game
+  boardButtons <- boardGUI board h f gameState
+  let gui = widgetise h boardButtons
+
+  quit <- button f [text := "Quit"
+                   ,on command := close f]
+  back <- button f [text := "Main"
+                   ,on command := (close f >> mainMenu)]
+
+  set f [layout := minsize (sz 200 100) $ margin 10 $ column 0 $ gui ++ [floatBottom $ widget back, floatBottom $ widget quit]
+        ,on click := \p -> let x = pointX p
+                               y = pointY p
+                           in putStrLn (show x ++ " " ++ show y)]
 
 main :: IO ()
-main = start mainMenu
+main = do
+  hSetBuffering stdout NoBuffering
+  start mainMenu
